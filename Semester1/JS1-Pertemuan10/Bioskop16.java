73de1669a9364d1cbe58c255b8e82ac6234b1561
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-02 01:59:25 +0700

    add foreach Penonton pada baris ke-3

--- Semester1/JS1-Pertemuan10/Bioskop16.java
+++ Semester1/JS1-Pertemuan10/Bioskop16.java
@@ -24,5 +24,9 @@ public class Bioskop16 {
         for (int i = 0; i < penonton[2].length; i++) {
             System.out.println(penonton[2][i]);     
         }
+        System.out.println("Penonton pada baris ke-3: ");
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
     }
 }
