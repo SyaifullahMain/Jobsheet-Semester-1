62ac6161380cb6198073231d2e5944b9d1a73a2e
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-08 07:18:29 +0700

    feat : create print"*" of N lines with length N using nested for

--- Semester1/JS1-Pertemuan11/Square16.java
+++ Semester1/JS1-Pertemuan11/Square16.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Square16 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan nilai N = ");
+        int N = sc.nextInt();
+        for (int Outer = 1; Outer <= N; Outer++) {
+            for (int i = 1; i <= N; i++) {
+                System.out.print("*");           
+            }
+            System.out.println();
+        }
     }
 }
