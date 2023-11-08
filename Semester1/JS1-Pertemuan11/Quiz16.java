f0da7b55ab2c0e2056e452d470b256beb693f302
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-08 09:01:22 +0700

    feat : add import Scanner class and Random class

--- Semester1/JS1-Pertemuan11/Quiz16.java
+++ Semester1/JS1-Pertemuan11/Quiz16.java
@@ -1,8 +1,11 @@
 /**
  * Quiz16
  */
+import java.util.Random;
+import java.util.Scanner;
 public class Quiz16 {
     public static void main(String[] args) {
-        
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
     }
 }
