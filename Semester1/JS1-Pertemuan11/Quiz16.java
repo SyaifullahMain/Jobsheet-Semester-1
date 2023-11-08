5114e4f3d8a546ce9a07519e43c673e796649a61
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-08 09:11:06 +0700

    feat : make a simple game of guessing numbers with Random Class using nasted do while

--- Semester1/JS1-Pertemuan11/Quiz16.java
+++ Semester1/JS1-Pertemuan11/Quiz16.java
@@ -1,11 +1,26 @@
+
 /**
  * Quiz16
  */
 import java.util.Random;
 import java.util.Scanner;
+
 public class Quiz16 {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner input = new Scanner(System.in);
+        char menu = 'Y';
+        do {
+            int number = random.nextInt(10) + 1;
+            boolean success = false;
+            do {
+                System.out.print("Tebak angka (1-10): ");
+                int answer = input.nextInt();
+                input.nextLine();
+                success = (answer == number);
+            } while (success);
+            System.out.println("Apakah Anda ingin mengulangi permainan (Y/y)");
+            menu = input.nextLine().charAt(0);
+        } while (menu == 'y' || menu == 'Y');
     }
 }
