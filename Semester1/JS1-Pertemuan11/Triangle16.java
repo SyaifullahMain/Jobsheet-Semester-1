d81beeb3b36e5e28fd2c0718e3fe2b56e808dcce
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-08 09:14:27 +0700

    frat : add Scanner class

--- Semester1/JS1-Pertemuan11/Triangle16.java
+++ Semester1/JS1-Pertemuan11/Triangle16.java
@@ -1,9 +1,11 @@
 /**
  * Triangle16
  */
+import java.util.Scanner;
 public class Triangle16 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
     }
 }
