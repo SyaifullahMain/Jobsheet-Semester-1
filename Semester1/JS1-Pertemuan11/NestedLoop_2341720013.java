8d381b5dd8d0862e48713010119e684d7a5319ec
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-08 11:13:30 +0700

    feat : holds temperature data for a city and displays it using nested for

--- Semester1/JS1-Pertemuan11/NestedLoop_2341720013.java
+++ Semester1/JS1-Pertemuan11/NestedLoop_2341720013.java
@@ -1,9 +1,28 @@
+
 /**
  * NestedLoop_2341720013
  */
+import java.util.Scanner;
+
 public class NestedLoop_2341720013 {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        double[][] temp = new double[5][7];
+        for (int i = 0; i < temp.length; i++) {
+            System.out.println("Kota ke-" + (i + 1));
+            for (int j = 0; j < temp[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + " : ");
+                temp[i][j] = scanner.nextDouble();
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < temp.length; i++) {
+            System.out.println("Kota ke-" + (i + 1));
+            for (int j = 0; j < temp[0].length; j++) {
+                System.out.println(temp[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
