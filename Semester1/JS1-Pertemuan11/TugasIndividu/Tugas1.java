b961bb77e7070f16ed990f60b58a131cec175f4c
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-09 10:10:41 +0700

    feat : Create a triangular print from numbers of length N

--- Semester1/JS1-Pertemuan11/TugasIndividu/Tugas1.java
+++ Semester1/JS1-Pertemuan11/TugasIndividu/Tugas1.java
@@ -1,7 +1,20 @@
 package TugasIndividu;
 
+import java.util.Scanner;
+
 public class Tugas1 {
   public static void main(String[] args) {
-    
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukan N = ");
+    int N = input.nextInt();
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= N-i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= i; j++) {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
   }
 }
