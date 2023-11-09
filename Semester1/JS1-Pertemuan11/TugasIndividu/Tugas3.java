603bf268e33250a5e83b072ceed185c7c793c078
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-09 10:55:26 +0700

    feat : create print* with a square shape of length N

--- Semester1/JS1-Pertemuan11/TugasIndividu/Tugas3.java
+++ Semester1/JS1-Pertemuan11/TugasIndividu/Tugas3.java
@@ -1,7 +1,18 @@
 package TugasIndividu;
 
+import java.util.Scanner;
+
 public class Tugas3 {
   public static void main(String[] args) {
-  
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukan N = ");
+    int N = input.nextInt();
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= N; j++) {
+        String _print = (i == 1 || i == N || j == 1 || j == N) ? String.valueOf(N)+" " : "  ";
+        System.out.print(_print);
+      }
+      System.out.println();
+    }
   }
 }
