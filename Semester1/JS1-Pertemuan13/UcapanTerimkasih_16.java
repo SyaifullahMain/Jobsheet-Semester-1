5a2f7f11b023dad222c01244d2848a094ce42b9c
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-21 23:58:32 +0700

    feat : Add function to get name of person and print thank you message

--- Semester1/JS1-Pertemuan13/UcapanTerimkasih_16.java
+++ Semester1/JS1-Pertemuan13/UcapanTerimkasih_16.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class UcapanTerimkasih_16 {
     public static void main(String[] args) {
-        
+        UcapanTerimaKasih();
+    }
+    public static String PenerimaUcapan() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan Nama orang yang akan diberi ucapan");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+    }
+    public static void UcapanTerimaKasih() {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you " + nama + " for being the best teacher in the world." + "You inspired in me a love for learning and made me feel like I could do ask you anything.");
     }
 }
