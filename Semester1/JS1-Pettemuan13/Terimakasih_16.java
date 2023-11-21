4726c962b6b36ea1493ef717b7de503b6b4e026f
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-21 21:55:30 +0700

    feat : make function without parameter

--- Semester1/JS1-Pettemuan13/Terimakasih_16.java
+++ Semester1/JS1-Pettemuan13/Terimakasih_16.java
@@ -1,5 +1,8 @@
 public class TerimaKasih_16 {
     public static void main(String[] args) {
-
+        UcapanTerimaKasih();
+    }
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world." + "You inspired in me a love for learning and made me feel like I could do ask you anything.");
     }    
 }
