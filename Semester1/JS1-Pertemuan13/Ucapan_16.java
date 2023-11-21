5d1c74cf768d99ee7a8c3d0850d757f6c61e6c08
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-21 23:06:02 +0700

    Add user input for recipient's name in Ucapan_16.java

--- Semester1/JS1-Pertemuan13/Ucapan_16.java
+++ Semester1/JS1-Pertemuan13/Ucapan_16.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Ucapan_16 {
     public static void main(String[] args) {
-        
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you " + nama + "\nMay the force be with you");
+    }
+    public static String PenerimaUcapan() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan Nama orang yang akan diberi ucapan");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
     }
 }
