839f8bb8ddfe2d8c130b2dbfedc847ec93858489
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-22 16:07:22 +0700

    feat : Add input and calculations for calculating the area and volume of a rectangle

--- Semester1/JS1-Pertemuan13/Percobaan6_16.java
+++ Semester1/JS1-Pertemuan13/Percobaan6_16.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Percobaan6_16 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int p, l, t, L, vol;
+        System.out.print("Masukan panjang: ");
+        p = input.nextInt();
+        System.out.print("Masukan lebar: ");
+        l = input.nextInt();
+        System.out.print("Masukan tinggi: ");
+        t = input.nextInt();
+        L = hitungLuas(p, l);
+        System.out.println("Luas Persegi Panjang adalah: " + L);
+        vol = hitungVolume(t, p, l);
+        System.out.println("Volume Balok adalah: " + vol);
+    }
+    static int hitungLuas (int pjg, int lb) {
+        int luas = pjg * lb;
+        return luas;
+    }
+    static int hitungVolume (int tinggi, int a, int b) {
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
     }
 }
