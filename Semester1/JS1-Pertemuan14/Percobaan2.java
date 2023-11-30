3e7eac38892c91ae306e0d8ce2739134b652298e
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-11-30 13:03:49 +0700

    Refactor hitungPangkat method to print each multiplication step

--- Semester1/JS1-Pertemuan14/Percobaan2.java
+++ Semester1/JS1-Pertemuan14/Percobaan2.java
@@ -6,9 +6,16 @@ public class Percobaan2 {
         int bilangan = sc.nextInt();
         System.out.print("Pangkat: ");
         int pangkat = sc.nextInt();
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        System.out.print(hitungPangkat(bilangan, pangkat));
     }
     static int hitungPangkat(int x, int y) {
-        return (y == 0) ? 1 : x * hitungPangkat(x, y - 1);
+        // return (y == 0) ? 1 : x * hitungPangkat(x, y - 1);
+        if (y == 0) {
+            System.out.print("1=");
+            return 1;
+        } else {
+            System.out.print(x + "x");
+            return x * hitungPangkat(x, y - 1);
+        }
     }
 }
